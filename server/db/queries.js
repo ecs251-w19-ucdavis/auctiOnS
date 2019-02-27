7e63ee2ae2841af4511d5063c8e667bb6ff67529
Kevin Wang <b01b01023@ntu.edu.tw>
2019-02-27 14:50:22 -0800

    Modify getItemInfo()

--- server/db/queries.js
+++ server/db/queries.js
@@ -9,7 +9,7 @@ module.exports = {
         getItemInfo: function() {
             // const curDate =  moment().add(1, 'day').format('YYYY-MM_DD')
             // const nextMonDate = moment().add(1, 'month').format('YYYY-MM_DD')
-            const item = knex('Item').where({id: 2})
+            const item = knex('Item').where({id: 1})
             return item
         }    
     }
