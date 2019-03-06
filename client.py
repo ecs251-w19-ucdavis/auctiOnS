7f9b89a3f17abbce313789f395af7f1f4a39730d
Daniel <dondon3345678@gmail.com>
2019-03-06 15:05:35 -0800

    Add argument parser and modify the auction logic.

--- client.py
+++ client.py
@@ -1,54 +1,52 @@
 import os, sys, time
 import requests as req
+import argparse
 from random import randint
 
-SERVER = 'http://localhost:3001'
-
-
 class client(object):
-    def __init__(self, name):
-        self.username           = name          # username for this client
-        self.server_id          = None          # which server the client is connect to
-        self.balance            = 10000.0       # current balance of the client, part of bidding decision factor
-        self.item_name          = None          # current item id that's in auction
-        self.current_price      = None           # current bid amount
+    def __init__(self, username, budget, server):
+        self.username           = username          # username for this client
+        self.budget             = budget            # current budget of the client, part of bidding decision factor
+        self.item_name          = None              # current item id that's in auction
+        self.current_price      = None              # current bid amount
         self.current_owner      = None          # current highest bid holder, a client id d
-        self.min_increment      = None           # current minimum increment of the item under auction
-        self.num_mi             = None          # number of minimum increment
+        self.increment          = None           # current increment of the item under auction
+        self.n_increment        = None          # number of increment
         self.bin_price          = None
+        self.SERVER             = server
+        print(f'''
+                Username:{self.username}
+                Budget: {self.budget}
+                Server: {self.SERVER}
+                ''')
         
 
     def show(self):
         # show client status
         print(f'''
-        	client id: 		{self.username}
-        	account balance:	{self.balance}
-        	current server: 	{self.server_id}
-        	current item: 		{self.item_name}
-        	current price: 		{self.current_price}
-        	current bid owner: 	{self.current_owner}
-        	current MI: 		{self.min_increment}
+        	Item: 		        {self.item_name}
+        	Current price: 		{self.current_price}
+        	Current bid owner: 	{self.current_owner}
+        	Current increment: 	{self.increment}
         	''')
     # Registers this user to server.
     def register(self):
-        self.receive(req.post(SERVER + '/register', data={'username' : self.username}).json())
+        self.receive(req.post(self.SERVER + '/register', data={'username' : self.username}).json())
 
     # (TODO) Decides whether to bid.
     def bid(self):
         if self.current_owner == self.username or \
-                self.balance < self.current_price + self.min_increment:
+                self.budget < self.current_price + self.increment:
             return False
         else:
-            max_inc = max(self.balance - self.current_price)
-            self.num_mi = randint(1, (self.balance - self.current_price) // self.min_increment)
             return True
 
     # (TODO) Executes the loop to decide to bid or go to sleep. 
     def bidding(self):
-        while self.receive(req.get(SERVER + '/getItemInfo').json()) is not None:
+        while self.receive('update', req.get(self.SERVER + '/getItemInfo').json()) is not None:
             if self.bid():
-                self.receive(req.post(SERVER + '/bid', data={\
-                        'username' : self.usernemae, \
+                self.receive('bid', req.post(self.SERVER + '/bid', data={\
+                        'username' : self.username, \
                         'current_price' : self.current_price, \
                         'num_mi' : self.num_mi}))
                 print('Bid!') 
@@ -58,38 +56,37 @@ class client(object):
         return
 
     # (TODO) Updates local information according to server response.
-    def receive(self, response):
-        # parse response
+    def receive(self, operation, response):
         if not response:
             print('Error response received.')
             return None
-        # registration confirmation
-        print(response)
-        if 'op' in response and response['op'] == 'register':
-            self.server_id = response['server_id']
-        # check listings, update internal information
-        elif 'current_price' in response:
-            print('getItem')
+        # Updates item information from server.
+        else:
+            print(response)
             self.current_price = response['current_price']
             self.current_owner = response['current_owner']
-            self.bin_price = response['bin_price']
-            self.min_increment = response['min_increment']
-            self.show()
-    	## bid accept
-        #elif op == 'bid':
-        #    self.current_item_owner = response['cur_item_owner']
-        #    self.current_price = response['cur_item_price'] # my_price should be parse from http response
-    	## new item notification
-        #elif op == 'new':
-        #    self.current_item = 'new_item'
-        #    self.current_price = 1000
-        #    self.cur_owner = 'cur_owner'
-        #    self.minimum_increment = 'MI'
-        #    # update account balance if won the bid
-        #    self.account_balance = 900
-        return True
+            if 'update' in operation:
+                print('getItemInfo')
+                self.bin_price = response['bin_price']
+                self.increment = response['increment']
+                self.show()
+    	    ## Handles bidding response.
+            elif 'bid' in operation:
+                if response['accept']:
+                    print('Bid accept')
+                else:
+                    print('Bid reject')
+            else:
+                print('Unknow operation.')
+
+def process_commands():
+    parser = argparse.ArgumentParser(description="Client of auctiOnS.")
+    parser.add_argument('--username', '-u', help='Name of this client.', required=True)
+    parser.add_argument('--budget', '-b', help='budget of this client', required=True)
+    parser.add_argument('--address', '-a', help='IP address of the server.', required=True)
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    c = client('testname')
-    c.register()
+    args = process_commands()
+    c = client(args.username, args.budget, args.address)
     #c.bidding()
