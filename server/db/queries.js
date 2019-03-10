c1cbec5b85f19e6c1f34a895ebbf52c2bb7b1590
Kevin Wang <b01b01023@ntu.edu.tw>
2019-03-09 22:49:33 -0800

    Add bidding function and modify code to ES7 syntax

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,20 +1,55 @@
 import knex from './knex';
 import moment from 'moment'
+import { rejects } from 'assert';
 
 module.exports = {
     Auction: {
-        checkClientName: function(name){
-            const client = knex('Client').where({client_name: name})
-            return client
-        },
-        register: function(name) {
-            return knex('Client').insert({client_name: name})
+        register:  function(name) {
+            return new Promise (async (resolve, reject)=>{
+                try{
+                    name = 'James'
+                    const client = await knex('Client').where({client_name: name})
+                    if(client.length) {
+                        reject(null)
+                    }
+                    else {
+                        await knex('Client').insert({client_name: name})
+                        resolve()
+                    }
+                }
+                catch(e){
+                    console.log(e)
+                }
+            })
         },
         getItemInfo: function() {
-            // const curDate =  moment().add(1, 'day').format('YYYY-MM_DD')
-            // const nextMonDate = moment().add(1, 'month').format('YYYY-MM_DD')
             const item = knex('Item').where({id: 1})
             return item
-        }    
+        },
+        bidding: function(name, current_price, num_mi) {
+            return new Promise ((resolve, reject) => {
+                knex.transaction( async function(t){
+                    try {
+                        const item = await knex('Item').where({id: 1})
+                        const current_price = item[0].current_price
+                        const bid_price = current_price + num_mi * item[0].min_increment
+
+                        if (bid_price > current_price && current_price < item[0].bin_price) {
+                            console.log('Succesful bid')
+                            await knex("Item").where({id: 1}).update({current_owner: name, current_price: bid_price});
+                            return resolve({'current_price' : bid_price, 'current_owner' : name, 'accept': true})
+                        }
+                        else {
+                            console.log('Unsuccessful bid')
+                            const d = {'current_price' : current_price, 'current_owner' : item[0].current_owner, 'accept': false}
+                            return reject(d)
+                        }
+                    }
+                    catch(e) {
+                        console.log(e)
+                    }
+                })
+            })
+        }  
     }
 }
--- server/routes/Auction.js
+++ server/routes/Auction.js
@@ -2,24 +2,41 @@ import express from 'express';
 import queries from '../db/queries';
 const router = express.Router();
 
-router.post('/register', (req, res) => {
-    queries.Auction.checkClientName(req.body.username).then(data => {
-        if(data.length) {
-            const d = {'op': 'register', 'success': false, 'server_id': 0}
-            res.json(d)
-        }
-        else {
-            queries.Auction.register(req.body.username).then(data => {
-                const d = {'op': 'register', 'success': true ,'server_id': 0}
-                res.json(d)
-            })
-        }
-    })
+router.post('/register', async (req, res) => {
+    let d = {'op': 'register', 'success': true ,'server_id': 0}
+    try {
+        await queries.Auction.register(req.body.username)
+        res.json(d)
+    }
+    catch(e) {
+        console.log(e)
+        d.success = false
+        res.json(d)
+    }
 })
-router.get('/getItemInfo', (req, res) => {
-    queries.Auction.getItemInfo().then(data => {
+router.get('/getItemInfo', async (req, res) => {
+    try {
+        const data = await queries.Auction.getItemInfo()
         res.json(data[0])
-    })
+    }
+    catch(e) {
+        console.log(e)
+    }
+})
+router.post('/bidding', async (req, res) => {
+    //{'username' : string , 'current_price' : string, 'num_mi' : string}
+    const name = res.body.username
+    const current_price = res.body.current_price
+    const num_mi = res.body.num_mi
+
+    try {
+        const d = await queries.Auction.bidding(name, current_price, num_mi)
+        res.json(d)
+    }
+    catch (e) {
+        console.log(e)
+        res.json(e)
+    }
 })
 
 module.exports = router;
